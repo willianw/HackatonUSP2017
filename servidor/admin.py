7520522c8d0a28ae8683af8bb07afdacd1d0e1fc
Willian Werner <willianwernerangelodacosta@gmail.com>
2017-08-19 15:34:04 -0700

    Banco de dados Django

--- servidor/admin.py
+++ servidor/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Pesquisador, Campus, Instituicao, GrandesAreas, MediasAreas, PequenasAreas, Pesquisa
 
 # Register your models here.
+admin.site.register(Pesquisador)
+admin.site.register(Campus)
+admin.site.register(Instituicao)
+admin.site.register(GrandesAreas)
+admin.site.register(MediasAreas)
+admin.site.register(PequenasAreas)
+admin.site.register(Pesquisa)
