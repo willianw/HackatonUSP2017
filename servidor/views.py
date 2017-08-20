9dd3ce85eba1d95e80b361405b42e3550e2fc3e2
Willian Werner <willianwernerangelodacosta@gmail.com>
2017-08-19 18:26:17 -0700

    Inicio simple_search

--- servidor/views.py
+++ servidor/views.py
@@ -2,10 +2,17 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
-def index(request):
-	context = client['database-hackaton'].professores.find_one()
+def simple_search(request):
+	query = 'FAU'
+	context = {
+		'status': 200,
+		'titulo': Pesquisa.objects.filter(nome__icontains=query),
+		'pequena_area': Pesquisa.objects.filter(pequena_area__nome__icontains=query),
+		'abstract': Pesquisa.objects.filter(abstract__icontains=query),
+		'tags': Pesquisa.objects.filter(tags__icontains=query),
+	}
 	print context
-	print context['nome']
 	return render(request, 'index.html', context)
